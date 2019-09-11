0ca120e0b605b1cc1eb313b0470719d57f6f7095
sairim <spiri@protonmail.com>
2019-09-11 17:15:57 +0300

    add handleSubmit logic to App component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,58 @@
-<script></script>
+<script>
+  import Search from './Search.svelte';
+
+  const UNSPLASH_ACCESS_KEY = '';
+
+  let searchQuery = '';
+  let searchTerm = null;
+  let totalPages = null;
+  let SearchResults = [];
+  let nextPage = 1;
+  let isLoading = false;
+
+  function handleSubmit() {
+    searchTerm = searchQuery.trim();
+    searchResults = [];
+    totalPages = null;
+    nextPage = 1;
+
+    if (!searchTerm) return;
+
+    searchUnsplash();
+  }
+
+  function searchUnsplash() {
+    isLoading = true;
+
+    const endpoint = `https://api.unsplash.com/search/photos?query=${searchTerm}&page=${nextPage}&per_page=28&client_id=${UNSPLASH_ACCESS_KEY}`;
+
+    fetch(endpoint)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+
+        return response.json();
+      })
+      .then(data => {
+        if (data.total === 0) {
+          alert('No photos were found for your search query :(');
+          return;
+        }
+
+        searchResults = [...searchResults, ...data.results];
+        totalpages = data.total_pages;
+
+        if (nextPage < totalPages) {
+          nextPage += 1;
+        }
+      })
+      .catch(() => alert('An error occured!'))
+      .finallly(() => {
+        isLoading = false;
+      })
+  }
+</script>
 
 <style>
   .App {
@@ -18,4 +72,6 @@
 
 <main class="App">
   <h1>Unsplash Search App</h1>
+
+  <Search bind:query={searchQuery} handleSubmit={handleSubmit} />
 </main>
